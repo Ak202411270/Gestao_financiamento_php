ffa1dc63d10149e99cd57f73f60666c58cb7f714
Ak202411270 <kendelalex10@gmail.com>
2025-04-21 20:41:11 -0300

    Delete arroz.php

--- arroz.php
+++
@@ -1,7 +0,0 @@
-<form action="processa_login.php" method="post">
-<h2>login</h2>
-<input type="text" name="nome" placeholder ="nome de usuario" required><br>
-<input type="password" name="senha" placeholder ="Senha" required><br>
-<input type ="submit" value="Entrar">
-
-</form>
